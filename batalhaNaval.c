bb01e34b6e362df4595f03f44e374831220fa891
JGABREU2004 <203727025+JGABREU2004@users.noreply.github.com>
2025-04-23 23:02:09 +0000

    Finalizando o desafio novato Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,12 +9,12 @@
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+// Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
+// Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
     // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
@@ -42,11 +42,30 @@ int main() {
 
     // iniciando o tabuleiro com 0 (água) e 3 (návio)
 
+    printf("  ## Tabuleiro Batalha Naval ##\n");
+    printf("\n");
+
     for (int i = 0; i < 10; i++) { // linhas
         for (int j = 0; j < 10; j++) // colunas
-        { tabuleiro[i][j] = 0;
-            
-        }        
+        { tabuleiro[i][j] = 0;}        
     }
-    return 0;
+    for (int i = 5; i < 5 + 3; i++) { // colocação do navio vertical
+        tabuleiro[i][6] = 3;
+    }   
+    for (int j = 3; j < 3 + 3; j++) { // colocação do navio na horizontal
+        tabuleiro[3][j] = 3;
+    }   
+        printf("   "); // espaço no canto superior
+
+    for (int j = 0; j < 10; j++) {
+        printf(" %c ", coluna[j]); // impressão coluna
+    }   printf("\n");
+
+    for (int i = 0; i < 10; i++) { // impressão do tabuleiro
+        printf("%2d ", i + 1);
+        for (int j = 0; j < 10; j++) {
+            printf(" %d ", tabuleiro[i][j]);
+        } printf ("\n");
+    } 
+    return 0; // fim do código
 }
