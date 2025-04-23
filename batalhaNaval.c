e287854bb7f55cd02d35576c47e9f9fc7a79c525
JGABREU2004 <203727025+JGABREU2004@users.noreply.github.com>
2025-04-23 20:40:48 +0000

    Iniciando o jogo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,8 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
+// Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
@@ -36,5 +35,18 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
+int main() {
+
+    char coluna[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    int tabuleiro[10][10];
+
+    // iniciando o tabuleiro com 0 (água) e 3 (návio)
+
+    for (int i = 0; i < 10; i++) { // linhas
+        for (int j = 0; j < 10; j++) // colunas
+        { tabuleiro[i][j] = 0;
+            
+        }        
+    }
     return 0;
 }
